56020d2f1719771bc60513551edf7dd096eaf504
petersen <pmphareng@gmail.com>
2019-05-04 16:07:22 +0200

    updated dev profile with bugs

---
+++ precache-manifest.bcf4490f1f5602852a11c83a1dfa9c70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d4c8cefe28baa7d897670ceb9266f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a0add2d0e5fdc40e71d",
+    "url": "/static/css/main.eae0323d.chunk.css"
+  },
+  {
+    "revision": "ab69fbb51b7e65d22c9d",
+    "url": "/static/js/2.5d47139a.chunk.js"
+  },
+  {
+    "revision": "3a0add2d0e5fdc40e71d",
+    "url": "/static/js/main.294cda72.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e7699e0060defeb8616c1b0caa822fb6",
+    "url": "/static/media/1.e7699e00.svg"
+  }
+]);
